203bfe9278905132aa2c05d08842025ced1070a1
Yampolskiy <v_yamp@mail.ru>
2020-05-25 13:09:35 +0300

    Машинное обучение - классификация

--- tools/machine_learning.py
+++ tools/machine_learning.py
@@ -25,6 +25,8 @@ import matplotlib.pyplot as plt
 class Ml:
     dir = Path('d:\\1')
     classes_file = dir / 'classes.pickle'
+    groups_file = dir / 'groups.pickle'
+    izgots_file = dir / 'izgots.pickle'
     model_file = dir / 'model_tovs.pickle'
     tovs_file = dir / 'tovs.csv'
     stop_words = get_stop_words('russian')
@@ -39,11 +41,16 @@ class Ml:
                       'Шкатулка керамическая',
                       'Кружка',
                       'Блюдо для блинов', '6/12 Набор кофейный 100мл', 'Кухонная вытяжка ELIKOR Альфа',
-                      'Изделие декоративное ', 'Молоток дверной Соня', 'Набор коробок', 'Брелок бабочка', 'Процессор']
+                      'Изделие декоративное ', 'Молоток дверной Соня', 'Набор коробок', 'Брелок бабочка']
 
-    def __init__(self, n=None) -> None:
+    cls = 1
+    izg = 2
+    grp = 3
+
+    def __init__(self, n=None, type=cls) -> None:
         super().__init__()
         self.n = n
+        self.type = type
 
     def connect_to_db(self):
         self.conn = psycopg2.connect(**self.dbc)
@@ -52,6 +59,8 @@ class Ml:
         print('get data from db')
         self.connect_to_db()
         self.load_classes_from_db(is_save_to_file)
+        self.load_groups_from_db(is_save_to_file)
+        self.load_izgots_from_db(is_save_to_file)
         self.load_tovs_from_db(is_save_to_file)
         self.make_train_data()
 
@@ -68,6 +77,14 @@ class Ml:
         with open(self.classes_file, 'rb') as f:
             self.classes = pickle.load(f)
 
+    def load_groups_from_file(self):
+        with open(self.groups_file, 'rb') as f:
+            self.classes = pickle.load(f)
+
+    def load_izgots_from_file(self):
+        with open(self.izgots_file, 'rb') as f:
+            self.izgots = pickle.load(f)
+
     def load_classes_from_db(self, is_save_to_file=True):
         frm = get_frame("select f_cod, f_name from spr_clas", connection=self.conn)
         self.classes = {frm.iloc[i]['f_cod']: frm.iloc[i]['f_name'] for i in range(len(frm.values))}
@@ -75,9 +92,23 @@ class Ml:
             with open(self.classes_file, 'wb') as f:
                 pickle.dump(self.classes, f)
 
+    def load_groups_from_db(self, is_save_to_file=True):
+        frm = get_frame("select f_cod, f_name from spr_grp", connection=self.conn)
+        self.groups = {frm.iloc[i]['f_cod']: frm.iloc[i]['f_name'] for i in range(len(frm.values))}
+        if is_save_to_file:
+            with open(self.groups_file, 'wb') as f:
+                pickle.dump(self.groups, f)
+
+    def load_izgots_from_db(self, is_save_to_file=True):
+        frm = get_frame("select f_cod, f_name from spr_pro", connection=self.conn)
+        self.izgots = {frm.iloc[i]['f_cod']: frm.iloc[i]['f_name'] for i in range(len(frm.values))}
+        if is_save_to_file:
+            with open(self.izgots_file, 'wb') as f:
+                pickle.dump(self.izgots, f)
+
     def load_tovs_from_db(self, is_save_to_file=True):
         sql = """
-        select r.f_name || ' IZG' || r.f_plant || ' GRP' || r.f_group || ' PRC' || round(r.f_price, -1) tov_name, r.f_class class_code
+        select r.f_name tov_name, r.f_class class_code, r.f_plant izgot_code, r.f_group group_code
         from rest r
         -- where r.f_cod between 0 and 50000
         """
@@ -90,8 +121,12 @@ class Ml:
             self.tovs_frm = self.tovs_frm.sample(n=self.n)
 
         self.X = self.tovs_frm['tov_name']
-        # self.X = self.transform_text(self.X)
-        self.y = self.tovs_frm['class_code']
+        if self.type == Ml.cls:
+            self.y = self.tovs_frm['class_code']
+        elif self.type == Ml.izg:
+            self.y = self.tovs_frm['izgot_code']
+        elif self.type == Ml.grp:
+            self.y = self.tovs_frm['group_code']
 
     def show_scatter(self):
         tfidf = TfidfVectorizer(analyzer='word', stop_words=Ml.stop_words, ngram_range=(1, 2))
@@ -102,7 +137,6 @@ class Ml:
         plt.scatter(data2D[:, 0], data2D[:, 1], c=ml.y, marker='o')
         # plt.scatter(data2D[:, 0], data2D[:, 1], c=['r' if i == 17 else 'b' for i in ml.y], marker='o')
         plt.show()
-
 
     # def transform_text(self, text_data):
     #     tfidf = TfidfVectorizer(analyzer='word', stop_words=self.stop_words, ngram_range=(1, 2))
@@ -128,6 +162,7 @@ class Ml:
     def define_data(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.3,
                                                                                 random_state=0)
+        # self.X_test = self.X_test.map(lambda n: n.split(":")[0])
 
     def fit(self, is_save_to_file=True, is_load_from_file=False):
         print('start fit')
@@ -151,13 +186,19 @@ class Ml:
 
     def predict_for_tov_name(self, tov_name):
         pred = self.model.predict([tov_name])
-        return self.classes[pred[0]]
+
+        if self.type == Ml.cls:
+            return self.classes[pred[0]]
+        elif self.type == Ml.izg:
+            return self.izgots[pred[0]]
+        elif self.type == Ml.grp:
+            return self.groups[pred[0]]
 
 
 if __name__ == '__main__':
-    is_load_data_from_db = False
+    is_load_data_from_db = True
     is_load_model_from_file = False
-    ml = Ml(n=None)
+    ml = Ml(n=None, type=Ml.cls)
     if is_load_model_from_file:
         ml.load_classes_from_file()
         ml.fit(is_load_from_file=is_load_model_from_file)
